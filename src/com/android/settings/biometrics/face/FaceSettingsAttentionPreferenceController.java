90aa8178b21eb026d4ad20bc9ba1b82dd3d63a17
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-26 04:56:47 -0300

    [4/4] Port face unlock feature
    * From motorola/foles_retail/foles:10/QPF30.130-15-7/38ece7:user/release-keys
    
    Change-Id: I906317e3d9c993fc5001ed9939dc25e67d3e636f

--- src/com/android/settings/biometrics/face/FaceSettingsAttentionPreferenceController.java
+++ src/com/android/settings/biometrics/face/FaceSettingsAttentionPreferenceController.java
@@ -28,6 +28,7 @@ import com.android.settings.Utils;
 import com.android.settings.core.TogglePreferenceController;
 
 import com.havoc.support.preferences.SwitchPreference;
+import com.android.internal.util.custom.faceunlock.FaceUnlockUtils;
 
 /**
  * Preference controller that manages the ability to use face authentication with/without
@@ -120,6 +121,9 @@ public class FaceSettingsAttentionPreferenceController extends FaceSettingsPrefe
 
     @Override
     public int getAvailabilityStatus() {
+        if (FaceUnlockUtils.hasMotoFaceUnlock()){
+            return UNSUPPORTED_ON_DEVICE;
+        }
         return AVAILABLE;
     }
 }
